13260eafbf1419a8b4556413fc1ca082b4f8fbe8
Sayyidshahab Nabavi <shahab.nab@gmail.com>
2026-08-10 00:45:00 +0200

    Add ML persistence package

---
+++ backend/ml/__init__.py
@@ -0,0 +1,5 @@
+"""Machine-learning persistence and model-registry helpers."""
+
+from backend.ml.persistence import ModelRegistry, ModelRegistryError, RunLogger
+
+__all__ = ["ModelRegistry", "ModelRegistryError", "RunLogger"]
